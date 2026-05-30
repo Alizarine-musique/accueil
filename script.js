3e5ae3e83f4194bf1dbf448c58f5da594d050e0d
Alizarine-musique <alizarinemusique@gmail.com>
2026-05-30 23:46:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ document.querySelectorAll(".concert").forEach(el => {
     el.classList.add("upcoming");
   }
 });
+
+/* METTRE EN AVANT LE PROCHAIN CONCERT*/
+const concerts = document.querySelectorAll(".concert");
+
+const now = new Date();
+
+let nextConcert = null;
+let nextDate = null;
+
+// 1. classer past / upcoming + trouver le prochain concert
+concerts.forEach(el => {
+  const date = new Date(el.dataset.date);
+
+  if (date < now) {
+    el.classList.add("past");
+  } else {
+    el.classList.add("upcoming");
+
+    // chercher le plus proche dans le futur
+    if (!nextDate || date < nextDate) {
+      nextDate = date;
+      nextConcert = el;
+    }
+  }
+});
+
+// 2. mettre en avant le prochain concert
+if (nextConcert) {
+  nextConcert.classList.add("next-concert");
+}
